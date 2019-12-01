3962319e8d8f8406b97856589773b9138cbdd7c4
Olav Stetter <olav.stetter@googlemail.com>
2019-12-01 16:49:05 +0100

    Added state monitoring to launch script.

--- start_instance.py
+++ start_instance.py
@@ -1,11 +1,22 @@
 import boto3
+import time
 
 session = boto3.session.Session()
 ec2 = session.resource('ec2')
 
-instances = ec2.create_instances(InstanceType='t1.micro',
+start_time = time.time()
+instances = ec2.create_instances(InstanceType='t2.small',
                                  ImageId='ami-0d4c3eabb9e72650a',
                                  MinCount=1,
                                  MaxCount=2)
 
 print(f'Created {len(instances)} EC2 instance(s).')
+
+for steps in range(1, 10):
+    print(f'\nAfter {(time.time() - start_time)/1e3} seconds:')
+    for instance in instances:
+        instance.reload()
+        id = instance.id
+        state = instance.state['Name']
+        print(f'- Instance ID {id} is in state {state}')
+    time.sleep(15)
